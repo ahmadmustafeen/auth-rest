08009edcac56bac210674418211bd531962282d7
Ahmad <engr.ahmadmustafeen@gmail.com>
2022-05-16 14:20:21 +0500

    cors package added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const { default: mongoose } = require("mongoose");
 const userSchema = require("./src/models/UserSchema");
 const userAuthRoutes = require("./src/routes/");
 const app = express();
+const cors = require("cors");
 
 const bodyParser = require("body-parser");
 require("dotenv").config();
@@ -10,10 +11,24 @@ const port = process.env.PORT || 3000;
 app.use(express.json({extended: true}))
 app.use(bodyParser.json());
 app.use(express.urlencoded());
+const corsOpts = {
+  origin: '*',
 
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
 const connect = mongoose.connect(process.env.MONGODB_PATH, {
   useNewUrlParser: true,
 });
+
 
 connect.then(
   () => {
